0dafaf1b127308dbe99c659e998258bd61aa2fae
valuvilla <valuvp@hotmail.com>
2022-10-17 00:35:43 +0200

    datos

--- pickle.py
+++ pickle.py
@@ -1,6 +1,17 @@
 import pickle
 
 
+datos=["Rosa","Verde"]
+
+
+ArchivoBinario=open("datos", "wb")
+
+pickle.dump(datos, ArchivoBinario)
+
+ArchivoBinario.close()
+
+
+
 Archivo2=open("datos", "rb")
 
 Lista2=pickle.load(Archivo2)
